dfb34eac154b55d672ee297b2dbe977829a9af1d
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2022-01-15 19:48:48 +0530

    ConcatenatingStrings.py

---
+++ ConcatenatingStrings.py
@@ -0,0 +1,13 @@
+
+def main():
+    testcases = int(input())
+    
+    while(testcases > 0):
+        string1 = input()
+        string2 = input()
+        print_fun(string1, string2)
+        
+        testcases -= 1
+
+if __name__ == '__main__':
+    main()
