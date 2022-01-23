1bd72d763e7562a981363e5f72c498b912a4a47d
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2022-01-23 15:47:52 +0530

    k_largestElements.java
    Given an array Arr of N positive integers, find K largest elements from the array.  The output elements should be printed in decreasing order.
    
    Example 1:
    
    Input:
    N = 5, K = 2
    Arr[] = {12, 5, 787, 1, 23}
    Output: 787 23

---
+++ k_largestElements.java
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int[] ans = new Solution().kLargest(arr, n, k);
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    int[] kLargest(int[] arr, int n, int k) {
+        // code here
+        Arrays.sort(arr);
+        int a[] = new int[k];
+        int l=0;
+        for(int i =n-1 ; i>n-k-1; i--){ 
+            a[l]=arr[i];
+            l=l+1;
+        }
+        return a;
+    }
+}
