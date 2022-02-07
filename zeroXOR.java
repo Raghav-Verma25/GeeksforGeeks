d19851250ef990001896d86ea1a9cb6a99c0d8a1
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2022-02-07 22:37:26 +0530

    Create zeroXOR.java
    Given an array A[] of size N. Find the number of pairs (i, j) such that
    Ai XOR Aj = 0, and 1 ≤ i < j ≤ N.

---
+++ zeroXOR.java
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    
+		    Complete obj = new Complete();
+		    long res = obj.calculate(arr, sizeOfArray);
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static long calculate (int arr[], int n) {
+        //Complete the function
+        int c=0;
+        for(int i =0 ; i< n-1 ; i++){ 
+            for(int j=1 ; j< n ; j++){ 
+                if( (arr[i]^arr[j])==0 && i<j){ 
+                    c=c+1;
+                }
+            }
+        }
+        return c;
+    }
+    
+    
+}
+
+
