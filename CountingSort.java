b2a80b0927a8997c118a80dd9b78071910ee92d0
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2022-02-14 23:17:29 +0530

    Create CountingSort.java
    Given a string arr consisting of lowercase english letters, arrange all its letters in lexicographical order using Counting Sort.
    
    Example 1:
    
    Input:
    N = 5
    S = "edsab"
    Output:
    abdes
    Explanation: 
    In lexicographical order, string will be 
    abdes.
    Example 2:
    
    Input:
    N = 13
    S = "geeksforgeeks"
    Output:
    eeeefggkkorss
    Explanation:
    In lexicographical order, string will be 
    eeeefggkkorss.

---
+++ CountingSort.java
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class CountSort
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            long n = sc.nextLong();
+            String arr = "";
+            arr = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.countSort(arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to arrange all letters of a string in lexicographical 
+    //order using Counting Sort.
+    public static String countSort(String arr)
+    {
+        // code here
+       char[] s2 = arr.toCharArray();
+       Arrays.sort(s2);
+       return new String (s2);
+    }
+}
