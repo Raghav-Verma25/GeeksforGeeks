8b8258f26c82069d6cc1c2ea9126e3e619d07114
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2022-03-16 13:57:48 +0530

    Create MultiplyString.java

---
+++ MultiplyString.java
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.math.*;
+
+class Multiply{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            String a=sc.next();
+            String b=sc.next();
+            Solution g=new Solution();
+            System.out.println(g.multiplyStrings(a,b));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String multiplyStrings(String s1,String s2)
+    {
+        //code here.
+        BigInteger x= new BigInteger(s1);
+        BigInteger y = new BigInteger(s2);
+        BigInteger res = x.multiply(y);
+        String res2 = String.valueOf(res);
+        return res2;
+        
+    }
+}
