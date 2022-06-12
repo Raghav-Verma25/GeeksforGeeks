75617c172bc019ae664f8d5a84958c692f14a345
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2022-06-12 15:24:50 +0530

    Create Longest consecutive subsequence.java

---
+++ Longest consecutive subsequence.java
@@ -0,0 +1,103 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			
+			for(int i=0; i<n; i++)
+				a[i] = sc.nextInt();
+		    out.println(new Solution().findLongestConseqSubseq(a, n));
+		    t--;
+		}
+		out.flush();
+	}
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{   
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+	static int findLongestConseqSubseq(int arr[], int N)
+	{
+	   // add your code here
+	   int res=1;
+	     Arrays.sort(arr);
+        ArrayList<Integer> arrlist = new ArrayList<>();
+        int sum =arr[0];
+        int j =1;
+        int max=0;
+        for(int i=0 ; i<N-1 ; i++){
+              if(arr[i]+1 == arr[i+1] ) {
+                j++;
+               
+            }
+            else if( arr[i]+1!= arr[i+1] && arr[i]!=arr[i+1]){
+                j=1;
+                
+            }
+          
+
+           res  = Math.max(res,j);
+
+        }
+   
+     return res;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
